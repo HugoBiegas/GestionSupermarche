5b0fe9ed4146d7e55df87090d633b19f572dce61
hugo biegas <hugo.biegas@sncf.fr>
2025-01-16 10:31:10 +0100

    Ajoutez des fichiers projet.

---
+++ GestionSupermarche/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionSupermarche
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GestionSupermarche/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace GestionSupermarche
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            // Initialisation de la base de données SQLite
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "supermarche.db");
+            //builder.Services.AddSingleton<Services.Database>(s => ActivatorUtilities.CreateInstance<Services.Database>(s, dbPath));
+
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
